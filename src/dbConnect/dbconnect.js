88851cea010ebb1f35bcc1a3ab89f758cc28f21d
Ayush Jha <ayushjmp108@gmail.com>
2022-03-29 08:41:19 +0530

    Added social media link and connected to monogodb atlas

--- src/dbConnect/dbconnect.js
+++ src/dbConnect/dbconnect.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const mongooseConnection = async()=>{
     try {
-        const connection = await mongoose.connect("mongodb://localhost:27017/Learner", {
+        const atlasUrl = 'mongodb+srv://ayushjha:ayushjha@cluster0.kclt5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+        const connection = await mongoose.connect(atlasUrl, {
             useNewUrlParser:true,
             useUnifiedTopology: true,
+            useUnifiedTopology:true,
+            
         });
         console.log("Connected to database...");
     } catch (error) {
